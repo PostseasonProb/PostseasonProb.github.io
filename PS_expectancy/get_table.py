a2c357b5e2a3b4d0ebcd71085931a82da32bf49e
dny <dnyeum@hanmail.net>
2021-09-02 22:32:28 +0900

    KBO PS expectancy calculating module for the 2021 season

--- PS_expectancy/get_table.py
+++ PS_expectancy/get_table.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import pathlib
+import datetime
 
 mname=['Jan.','Feb.','Mar.','Apr.','May','Jun.','Jul.','Aug.','Sep.','Oct.','Nov.','Dec.']
 team = ['두산','롯데','삼성','키움','한화','KIA','KT','LG','NC','SSG']
@@ -46,7 +47,7 @@ for x in range (noteams):
     res_df['PS_odds'][x] = format(PS_odd[x][length-1]*100.,'.1f')+'%'
     res_df['기대패배'][x] = res_df['경기'][x]-res_df['기대승리'][x]-res_df['무승부'][x]
     res_df['기대승률'][x]=res_df['기대승리'][x]/(res_df['기대승리'][x]+res_df['기대패배'][x])
-    res_df['무승부'][x] = format(res_df['무승부'][x],'.1f')
+    res_df['무승부'][x] = res_df['무승부'][x]
     res_df['기대승리'][x] = format(res_df['기대승리'][x],'.1f')
     res_df['기대패배'][x] = format(res_df['기대패배'][x],'.1f')
     res_df['기대승률'][x] = format(res_df['기대승률'][x],'.3f')
@@ -54,4 +55,25 @@ res_df = res_df.sort_values(by=['기대승률'],axis=0,ascending=False)
 res_df = res_df.reset_index(drop=True)
 res_df.index+=1
 res_df.index.name='기대순위'
-res_df.to_csv('expected.csv')
+
+today = datetime.datetime.now()-datetime.timedelta(days=1)
+
+year = today.year
+month = today.month
+day = today.day
+
+length = len(res_df)
+
+oFile = open(str(pathlib.Path().resolve())+"/../rank_table.html","w")
+oFile.write("<h2> 2021시즌 기대 순위 </h2>\n")
+oFile.write("<p>"+str(year)+"년 "+str(month)+"월 "+str(day)+"일까지</p><br>\n")
+oFile.write("<table>\n")
+oFile.write("<tr>\n")
+oFile.write("<th>기대순위</th> <th>팀</th> <th>기대승리</th> <th>무승부</th> <th>기대패배</th> <th>기대승률</th> <th>PS_odds</th>\n")
+oFile.write("</tr>\n")
+for x in range (1,length+1):
+        oFile.write("<tr>\n")
+        oFile.write("<td>"+str(x)+"</td> <td>"+str(res_df.loc[x,'팀'])+"</td> <td>"+str(res_df.loc[x,'기대승리'])+"</td> <td>"+str(res_df.loc[x,'무승부'])+"</td> <td>"+str(res_df.loc[x,'기대패배'])+"</td> <td>"+str(res_df.loc[x,'기대승률'])+"</td> <td>"+str(res_df.loc[x,'PS_odds'])+"</td>\n")
+        oFile.write("</tr>\n")
+oFile.write("</table>")
+oFile.close()
