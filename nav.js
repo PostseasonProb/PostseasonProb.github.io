f95cf4a9714c901cc187273d624696ab0776fd28
dny <dnyeum@hanmail.net>
2021-10-02 00:11:57 +0900

    2021.10.01

---
+++ nav.js
@@ -0,0 +1,5 @@
+﻿window.onload=function() {
+  console.log("gheheh");
+  var strHead = '<nav>\n  <link rel="stylesheet" href="StyleSheet.css">\n  <ul>\n    <li>\n      <a href="https://postseasonprob.github.io/">Home</a>\n    </li>\n    <li><a href="https://postseasonprob.github.io/GuideTo.html">사용설명서</a>\n    </li>\n    <li>\n      <a href="https://postseasonprob.github.io/2021.html">시즌별 일람</a>\n    </li>\n  </ul>\n</nav>'
+  document.getElementById("navHeader").innerHTML=strHead;
+}
