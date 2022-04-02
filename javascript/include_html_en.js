445771f7f4514ca1b2312606fb5fe44298ddb1b1
dny <dnyeum@hanmail.net>
2022-04-02 23:31:45 +0900

    2022.04.02

--- javascript/include_html_en.js
+++ javascript/include_html_en.js
@@ -1,6 +1,6 @@
 window.onload=function() {
 
-  var strHead = '<nav>\n  <ul>\n    <li>\n      <a href="https://postseasonprob.github.io/en/index_en.html">Home</a>\n    </li>\n    <li><a href="https://postseasonprob.github.io/en/GuideTo_en.html">User Guide</a>\n    </li>\n    <li>\n      <a href="https://postseasonprob.github.io/en/2021_en.html">Season Summary</a>\n    </li>\n  </ul>\n</nav>'
+  var strHead = '<nav>\n  <ul>\n    <li>\n      <a href="https://postseasonprob.github.io/en/index_en.html">Home</a>\n    </li>\n    <li><a href="https://postseasonprob.github.io/en/GuideTo_en.html">User Guide</a>\n    </li>\n    <li>\n      <a href="https://postseasonprob.github.io/en/2022_en.html">Season Summary</a>\n    </li>\n  </ul>\n</nav>'
 
   var strYear = '<option value="none">-Season-</option>\n';
   for (var i=2022; i>=2001; i--) {
