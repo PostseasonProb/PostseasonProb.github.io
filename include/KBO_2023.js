152f82f5f2cbcd558a181b294047c88555368fa5
dnyeum <dnyeum@hanmail.net>
2023-04-03 00:06:23 +0900

    2023.04.02

--- include/KBO_2023.js
+++ include/KBO_2023.js
@@ -1,70 +1,70 @@
-var xRange = ["2023-03-31", "2023-04-01"];
+var xRange = ["2023-03-31", "2023-04-01", "2023-04-02"];
 var PS1= {
   x: xRange,
-  y: [0.5,0.5388769],
+  y: [0.5,0.5388769,0.5],
   mode: "lines",
   name: "두산",
   line: {color:"#000080", width: 4}
 };
 var PS2= {
   x: xRange,
-  y: [0.5,0.4711257],
+  y: [0.5,0.4711257,0.5],
   mode: "lines",
   name: "롯데",
   line: {color:"#87ceeb", width: 4}
 };
 var PS3= {
   x: xRange,
-  y: [0.5,0.45838539999999994],
+  y: [0.5,0.45838539999999994,0.5],
   mode: "lines",
   name: "삼성",
   line: {color:"#074ca1", width: 4}
 };
 var PS4= {
   x: xRange,
-  y: [0.5,0.5249159999999999],
+  y: [0.5,0.5249159999999999,0.5],
   mode: "lines",
   name: "키움",
   line: {color:"#820024", width: 4}
 };
 var PS5= {
   x: xRange,
-  y: [0.5,0.4739209],
+  y: [0.5,0.4739209,0.5],
   mode: "lines",
   name: "한화",
   line: {color:"#ff6600", width: 4}
 };
 var PS6= {
   x: xRange,
-  y: [0.5,0.46460019999999996],
+  y: [0.5,0.46460019999999996,0.5],
   mode: "lines",
   name: "KIA",
   line: {color:"#ea0029", width: 4}
 };
 var PS7= {
   x: xRange,
-  y: [0.5,0.5350488],
+  y: [0.5,0.5350488,0.5],
   mode: "lines",
   name: "KT",
   line: {color:"#000000", width: 4}
 };
 var PS8= {
   x: xRange,
-  y: [0.5,0.46134830000000004],
+  y: [0.5,0.46134830000000004,0.5],
   mode: "lines",
   name: "LG",
   line: {color:"#c30452", width: 4}
 };
 var PS9= {
   x: xRange,
-  y: [0.5,0.5331824000000001],
+  y: [0.5,0.5331824000000001,0.5],
   mode: "lines",
   name: "NC",
   line: {color:"#315288", width: 4}
 };
 var PS10= {
   x: xRange,
-  y: [0.5,0.5385961],
+  y: [0.5,0.5385961,0.5],
   mode: "lines",
   name: "SSG",
   line: {color:"#ff0000", width: 4}
@@ -73,70 +73,70 @@ var dataPS = [PS1, PS2, PS3, PS4, PS5, PS6, PS7, PS8, PS9, PS10]
 
 var KS1= {
   x: xRange,
-  y: [0.1,0.114791],
+  y: [0.1,0.114791,0.1],
   mode: "lines",
   name: "두산",
   line: {color:"#000080", width: 4}
 };
 var KS2= {
   x: xRange,
-  y: [0.1,0.0893516],
+  y: [0.1,0.0893516,0.1],
   mode: "lines",
   name: "롯데",
   line: {color:"#87ceeb", width: 4}
 };
 var KS3= {
   x: xRange,
-  y: [0.1,0.0830757],
+  y: [0.1,0.0830757,0.1],
   mode: "lines",
   name: "삼성",
   line: {color:"#074ca1", width: 4}
 };
 var KS4= {
   x: xRange,
-  y: [0.1,0.110656],
+  y: [0.1,0.110656,0.1],
   mode: "lines",
   name: "키움",
   line: {color:"#820024", width: 4}
 };
 var KS5= {
   x: xRange,
-  y: [0.1,0.0887347],
+  y: [0.1,0.0887347,0.1],
   mode: "lines",
   name: "한화",
   line: {color:"#ff6600", width: 4}
 };
 var KS6= {
   x: xRange,
-  y: [0.1,0.085106],
+  y: [0.1,0.085106,0.1],
   mode: "lines",
   name: "KIA",
   line: {color:"#ea0029", width: 4}
 };
 var KS7= {
   x: xRange,
-  y: [0.1,0.115615],
+  y: [0.1,0.115615,0.1],
   mode: "lines",
   name: "KT",
   line: {color:"#000000", width: 4}
 };
 var KS8= {
   x: xRange,
-  y: [0.1,0.0833874],
+  y: [0.1,0.0833874,0.1],
   mode: "lines",
   name: "LG",
   line: {color:"#c30452", width: 4}
 };
 var KS9= {
   x: xRange,
-  y: [0.1,0.11302],
+  y: [0.1,0.11302,0.1],
   mode: "lines",
   name: "NC",
   line: {color:"#315288", width: 4}
 };
 var KS10= {
   x: xRange,
-  y: [0.1,0.11626299999999999],
+  y: [0.1,0.11626299999999999,0.1],
   mode: "lines",
   name: "SSG",
   line: {color:"#ff0000", width: 4}
@@ -145,70 +145,70 @@ var dataKS = [KS1, KS2, KS3, KS4, KS5, KS6, KS7, KS8, KS9, KS10]
 
 var PO1= {
   x: xRange,
-  y: [0.1,0.0957579],
+  y: [0.1,0.0957579,0.1],
   mode: "lines",
   name: "두산",
   line: {color:"#000080", width: 4}
 };
 var PO2= {
   x: xRange,
-  y: [0.1,0.0813657],
+  y: [0.1,0.0813657,0.1],
   mode: "lines",
   name: "롯데",
   line: {color:"#87ceeb", width: 4}
 };
 var PO3= {
   x: xRange,
-  y: [0.1,0.08297270000000001],
+  y: [0.1,0.08297270000000001,0.1],
   mode: "lines",
   name: "삼성",
   line: {color:"#074ca1", width: 4}
 };
 var PO4= {
   x: xRange,
-  y: [0.1,0.102979],
+  y: [0.1,0.102979,0.1],
   mode: "lines",
   name: "키움",
   line: {color:"#820024", width: 4}
 };
 var PO5= {
   x: xRange,
-  y: [0.1,0.09092700000000001],
+  y: [0.1,0.09092700000000001,0.1],
   mode: "lines",
   name: "한화",
   line: {color:"#ff6600", width: 4}
 };
 var PO6= {
   x: xRange,
-  y: [0.1,0.0914393],
+  y: [0.1,0.0914393,0.1],
   mode: "lines",
   name: "KIA",
   line: {color:"#ea0029", width: 4}
 };
 var PO7= {
   x: xRange,
-  y: [0.1,0.113878],
+  y: [0.1,0.113878,0.1],
   mode: "lines",
   name: "KT",
   line: {color:"#000000", width: 4}
 };
 var PO8= {
   x: xRange,
-  y: [0.1,0.0970445],
+  y: [0.1,0.0970445,0.1],
   mode: "lines",
   name: "LG",
   line: {color:"#c30452", width: 4}
 };
 var PO9= {
   x: xRange,
-  y: [0.1,0.11973],
+  y: [0.1,0.11973,0.1],
   mode: "lines",
   name: "NC",
   line: {color:"#315288", width: 4}
 };
 var PO10= {
   x: xRange,
-  y: [0.1,0.123906],
+  y: [0.1,0.123906,0.1],
   mode: "lines",
   name: "SSG",
   line: {color:"#ff0000", width: 4}
@@ -217,70 +217,70 @@ var dataPO = [PO1, PO2, PO3, PO4, PO5, PO6, PO7, PO8, PO9, PO10]
 
 var SP1= {
   x: xRange,
-  y: [0.1,0.104778],
+  y: [0.1,0.104778,0.1],
   mode: "lines",
   name: "두산",
   line: {color:"#000080", width: 4}
 };
 var SP2= {
   x: xRange,
-  y: [0.1,0.0933082],
+  y: [0.1,0.0933082,0.1],
   mode: "lines",
   name: "롯데",
   line: {color:"#87ceeb", width: 4}
 };
 var SP3= {
   x: xRange,
-  y: [0.1,0.09182649999999999],
+  y: [0.1,0.09182649999999999,0.1],
   mode: "lines",
   name: "삼성",
   line: {color:"#074ca1", width: 4}
 };
 var SP4= {
   x: xRange,
-  y: [0.1,0.103281],
+  y: [0.1,0.103281,0.1],
   mode: "lines",
   name: "키움",
   line: {color:"#820024", width: 4}
 };
 var SP5= {
   x: xRange,
-  y: [0.1,0.0963277],
+  y: [0.1,0.0963277,0.1],
   mode: "lines",
   name: "한화",
   line: {color:"#ff6600", width: 4}
 };
 var SP6= {
   x: xRange,
-  y: [0.1,0.0934713],
+  y: [0.1,0.0934713,0.1],
   mode: "lines",
   name: "KIA",
   line: {color:"#ea0029", width: 4}
 };
 var SP7= {
   x: xRange,
-  y: [0.1,0.106255],
+  y: [0.1,0.106255,0.1],
   mode: "lines",
   name: "KT",
   line: {color:"#000000", width: 4}
 };
 var SP8= {
   x: xRange,
-  y: [0.1,0.09403270000000001],
+  y: [0.1,0.09403270000000001,0.1],
   mode: "lines",
   name: "LG",
   line: {color:"#c30452", width: 4}
 };
 var SP9= {
   x: xRange,
-  y: [0.1,0.107444],
+  y: [0.1,0.107444,0.1],
   mode: "lines",
   name: "NC",
   line: {color:"#315288", width: 4}
 };
 var SP10= {
   x: xRange,
-  y: [0.1,0.109276],
+  y: [0.1,0.109276,0.1],
   mode: "lines",
   name: "SSG",
   line: {color:"#ff0000", width: 4}
@@ -289,70 +289,70 @@ var dataSP = [SP1, SP2, SP3, SP4, SP5, SP6, SP7, SP8, SP9, SP10]
 
 var WINS1= {
   x: xRange,
-  y: [72.0,72.5943],
+  y: [72.0,72.5943,72.0],
   mode: "lines",
   name: "두산",
   line: {color:"#000080", width: 4}
 };
 var WINS2= {
   x: xRange,
-  y: [72.0,71.5899],
+  y: [72.0,71.5899,72.0],
   mode: "lines",
   name: "롯데",
   line: {color:"#87ceeb", width: 4}
 };
 var WINS3= {
   x: xRange,
-  y: [72.0,71.3643],
+  y: [72.0,71.3643,72.0],
   mode: "lines",
   name: "삼성",
   line: {color:"#074ca1", width: 4}
 };
 var WINS4= {
   x: xRange,
-  y: [72.0,72.4147],
+  y: [72.0,72.4147,72.0],
   mode: "lines",
   name: "키움",
   line: {color:"#820024", width: 4}
 };
 var WINS5= {
   x: xRange,
-  y: [72.0,71.5852],
+  y: [72.0,71.5852,72.0],
   mode: "lines",
   name: "한화",
   line: {color:"#ff6600", width: 4}
 };
 var WINS6= {
   x: xRange,
-  y: [72.0,71.4449],
+  y: [72.0,71.4449,72.0],
   mode: "lines",
   name: "KIA",
   line: {color:"#ea0029", width: 4}
 };
 var WINS7= {
   x: xRange,
-  y: [72.0,72.5417],
+  y: [72.0,72.5417,72.0],
   mode: "lines",
   name: "KT",
   line: {color:"#000000", width: 4}
 };
 var WINS8= {
   x: xRange,
-  y: [72.0,71.3969],
+  y: [72.0,71.3969,72.0],
   mode: "lines",
   name: "LG",
   line: {color:"#c30452", width: 4}
 };
 var WINS9= {
   x: xRange,
-  y: [72.0,72.5129],
+  y: [72.0,72.5129,72.0],
   mode: "lines",
   name: "NC",
   line: {color:"#315288", width: 4}
 };
 var WINS10= {
   x: xRange,
-  y: [72.0,72.5552],
+  y: [72.0,72.5552,72.0],
   mode: "lines",
   name: "SSG",
   line: {color:"#ff0000", width: 4}
