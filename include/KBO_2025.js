220f4f79533ba2a7dc59e05ee63513572394f0ae
dnyeum <dnyeum@hanmail.net>
2025-03-23 00:01:36 +0900

    2025.03.22

--- include/KBO_2025.js
+++ include/KBO_2025.js
@@ -1,70 +1,70 @@
-var xRange = ["2025-03-21"];
+var xRange = ["2025-03-21", "2025-03-22"];
 var PS1= {
   x: xRange,
-  y: [0.4891813],
+  y: [0.4891813,0.45830160000000003],
   mode: "lines",
   name: "두산",
   line: {color:"#000080", width: 4}
 };
 var PS2= {
   x: xRange,
-  y: [0.5194230000000001],
+  y: [0.5194230000000001,0.48757],
   mode: "lines",
   name: "롯데",
   line: {color:"#87ceeb", width: 4}
 };
 var PS3= {
   x: xRange,
-  y: [0.514857],
+  y: [0.514857,0.548426],
   mode: "lines",
   name: "삼성",
   line: {color:"#074ca1", width: 4}
 };
 var PS4= {
   x: xRange,
-  y: [0.47967800000000005],
+  y: [0.47967800000000005,0.45044979999999996],
   mode: "lines",
   name: "키움",
   line: {color:"#820024", width: 4}
 };
 var PS5= {
   x: xRange,
-  y: [0.5029830000000001],
+  y: [0.5029830000000001,0.5340469999999999],
   mode: "lines",
   name: "한화",
   line: {color:"#ff6600", width: 4}
 };
 var PS6= {
   x: xRange,
-  y: [0.499135],
+  y: [0.499135,0.526836],
   mode: "lines",
   name: "KIA",
   line: {color:"#ea0029", width: 4}
 };
 var PS7= {
   x: xRange,
-  y: [0.508087],
+  y: [0.508087,0.47638300000000006],
   mode: "lines",
   name: "KT",
   line: {color:"#000000", width: 4}
 };
 var PS8= {
   x: xRange,
-  y: [0.48640809999999995],
+  y: [0.48640809999999995,0.5183315000000001],
   mode: "lines",
   name: "LG",
   line: {color:"#c30452", width: 4}
 };
 var PS9= {
   x: xRange,
-  y: [0.4868719],
+  y: [0.4868719,0.4582585],
   mode: "lines",
   name: "NC",
   line: {color:"#af917b", width: 4}
 };
 var PS10= {
   x: xRange,
-  y: [0.513375],
+  y: [0.513375,0.541396],
   mode: "lines",
   name: "SSG",
   line: {color:"#ffb81c", width: 4}
@@ -73,70 +73,70 @@ var dataPS = [PS1, PS2, PS3, PS4, PS5, PS6, PS7, PS8, PS9, PS10]
 
 var KS1= {
   x: xRange,
-  y: [0.09502469999999999],
+  y: [0.09502469999999999,0.0814376],
   mode: "lines",
   name: "두산",
   line: {color:"#000080", width: 4}
 };
 var KS2= {
   x: xRange,
-  y: [0.107921],
+  y: [0.107921,0.09467040000000002],
   mode: "lines",
   name: "롯데",
   line: {color:"#87ceeb", width: 4}
 };
 var KS3= {
   x: xRange,
-  y: [0.10433],
+  y: [0.10433,0.119921],
   mode: "lines",
   name: "삼성",
   line: {color:"#074ca1", width: 4}
 };
 var KS4= {
   x: xRange,
-  y: [0.09222860000000001],
+  y: [0.09222860000000001,0.0800605],
   mode: "lines",
   name: "키움",
   line: {color:"#820024", width: 4}
 };
 var KS5= {
   x: xRange,
-  y: [0.0996667],
+  y: [0.0996667,0.11381],
   mode: "lines",
   name: "한화",
   line: {color:"#ff6600", width: 4}
 };
 var KS6= {
   x: xRange,
-  y: [0.0995459],
+  y: [0.0995459,0.11125],
   mode: "lines",
   name: "KIA",
   line: {color:"#ea0029", width: 4}
 };
 var KS7= {
   x: xRange,
-  y: [0.103647],
+  y: [0.103647,0.0889055],
   mode: "lines",
   name: "KT",
   line: {color:"#000000", width: 4}
 };
 var KS8= {
   x: xRange,
-  y: [0.09526860000000001],
+  y: [0.09526860000000001,0.106848],
   mode: "lines",
   name: "LG",
   line: {color:"#c30452", width: 4}
 };
 var KS9= {
   x: xRange,
-  y: [0.096484],
+  y: [0.096484,0.0834286],
   mode: "lines",
   name: "NC",
   line: {color:"#af917b", width: 4}
 };
 var KS10= {
   x: xRange,
-  y: [0.10588399999999999],
+  y: [0.10588399999999999,0.119669],
   mode: "lines",
   name: "SSG",
   line: {color:"#ffb81c", width: 4}
@@ -145,70 +145,70 @@ var dataKS = [KS1, KS2, KS3, KS4, KS5, KS6, KS7, KS8, KS9, KS10]
 
 var PO1= {
   x: xRange,
-  y: [0.09834530000000001],
+  y: [0.09834530000000001,0.0886124],
   mode: "lines",
   name: "두산",
   line: {color:"#000080", width: 4}
 };
 var PO2= {
   x: xRange,
-  y: [0.104159],
+  y: [0.104159,0.0946996],
   mode: "lines",
   name: "롯데",
   line: {color:"#87ceeb", width: 4}
 };
 var PO3= {
   x: xRange,
-  y: [0.1044],
+  y: [0.1044,0.112229],
   mode: "lines",
   name: "삼성",
   line: {color:"#074ca1", width: 4}
 };
 var PO4= {
   x: xRange,
-  y: [0.0938014],
+  y: [0.0938014,0.0871495],
   mode: "lines",
   name: "키움",
   line: {color:"#820024", width: 4}
 };
 var PO5= {
   x: xRange,
-  y: [0.0987633],
+  y: [0.0987633,0.1095],
   mode: "lines",
   name: "한화",
   line: {color:"#ff6600", width: 4}
 };
 var PO6= {
   x: xRange,
-  y: [0.0989141],
+  y: [0.0989141,0.10688],
   mode: "lines",
   name: "KIA",
   line: {color:"#ea0029", width: 4}
 };
 var PO7= {
   x: xRange,
-  y: [0.10263299999999999],
+  y: [0.10263299999999999,0.09341450000000001],
   mode: "lines",
   name: "KT",
   line: {color:"#000000", width: 4}
 };
 var PO8= {
   x: xRange,
-  y: [0.09631139999999999],
+  y: [0.09631139999999999,0.106702],
   mode: "lines",
   name: "LG",
   line: {color:"#c30452", width: 4}
 };
 var PO9= {
   x: xRange,
-  y: [0.097986],
+  y: [0.097986,0.08990139999999999],
   mode: "lines",
   name: "NC",
   line: {color:"#af917b", width: 4}
 };
 var PO10= {
   x: xRange,
-  y: [0.104686],
+  y: [0.104686,0.11091100000000001],
   mode: "lines",
   name: "SSG",
   line: {color:"#ffb81c", width: 4}
@@ -217,70 +217,70 @@ var dataPO = [PO1, PO2, PO3, PO4, PO5, PO6, PO7, PO8, PO9, PO10]
 
 var SP1= {
   x: xRange,
-  y: [0.09855],
+  y: [0.09855,0.09251],
   mode: "lines",
   name: "두산",
   line: {color:"#000080", width: 4}
 };
 var SP2= {
   x: xRange,
-  y: [0.10312],
+  y: [0.10312,0.09803],
   mode: "lines",
   name: "롯데",
   line: {color:"#87ceeb", width: 4}
 };
 var SP3= {
   x: xRange,
-  y: [0.10231],
+  y: [0.10231,0.10872],
   mode: "lines",
   name: "삼성",
   line: {color:"#074ca1", width: 4}
 };
 var SP4= {
   x: xRange,
-  y: [0.09661],
+  y: [0.09661,0.0912],
   mode: "lines",
   name: "키움",
   line: {color:"#820024", width: 4}
 };
 var SP5= {
   x: xRange,
-  y: [0.10233],
+  y: [0.10233,0.10694],
   mode: "lines",
   name: "한화",
   line: {color:"#ff6600", width: 4}
 };
 var SP6= {
   x: xRange,
-  y: [0.09906],
+  y: [0.09906,0.10533],
   mode: "lines",
   name: "KIA",
   line: {color:"#ea0029", width: 4}
 };
 var SP7= {
   x: xRange,
-  y: [0.10041],
+  y: [0.10041,0.09509],
   mode: "lines",
   name: "KT",
   line: {color:"#000000", width: 4}
 };
 var SP8= {
   x: xRange,
-  y: [0.09777],
+  y: [0.09777,0.1038],
   mode: "lines",
   name: "LG",
   line: {color:"#c30452", width: 4}
 };
 var SP9= {
   x: xRange,
-  y: [0.09739],
+  y: [0.09739,0.09044],
   mode: "lines",
   name: "NC",
   line: {color:"#af917b", width: 4}
 };
 var SP10= {
   x: xRange,
-  y: [0.10245],
+  y: [0.10245,0.10794],
   mode: "lines",
   name: "SSG",
   line: {color:"#ffb81c", width: 4}
@@ -289,70 +289,70 @@ var dataSP = [SP1, SP2, SP3, SP4, SP5, SP6, SP7, SP8, SP9, SP10]
 
 var WINS1= {
   x: xRange,
-  y: [71.8196],
+  y: [71.8196,71.3345],
   mode: "lines",
   name: "두산",
   line: {color:"#000080", width: 4}
 };
 var WINS2= {
   x: xRange,
-  y: [72.2826],
+  y: [72.2826,71.7809],
   mode: "lines",
   name: "롯데",
   line: {color:"#87ceeb", width: 4}
 };
 var WINS3= {
   x: xRange,
-  y: [72.2393],
+  y: [72.2393,72.7257],
   mode: "lines",
   name: "삼성",
   line: {color:"#074ca1", width: 4}
 };
 var WINS4= {
   x: xRange,
-  y: [71.6694],
+  y: [71.6694,71.2221],
   mode: "lines",
   name: "키움",
   line: {color:"#820024", width: 4}
 };
 var WINS5= {
   x: xRange,
-  y: [72.0318],
+  y: [72.0318,72.5404],
   mode: "lines",
   name: "한화",
   line: {color:"#ff6600", width: 4}
 };
 var WINS6= {
   x: xRange,
-  y: [71.978],
+  y: [71.978,72.425],
   mode: "lines",
   name: "KIA",
   line: {color:"#ea0029", width: 4}
 };
 var WINS7= {
   x: xRange,
-  y: [72.1235],
+  y: [72.1235,71.6228],
   mode: "lines",
   name: "KT",
   line: {color:"#000000", width: 4}
 };
 var WINS8= {
   x: xRange,
-  y: [71.7958],
+  y: [71.7958,72.2871],
   mode: "lines",
   name: "LG",
   line: {color:"#c30452", width: 4}
 };
 var WINS9= {
   x: xRange,
-  y: [71.8327],
+  y: [71.8327,71.3671],
   mode: "lines",
   name: "NC",
   line: {color:"#af917b", width: 4}
 };
 var WINS10= {
   x: xRange,
-  y: [72.2272],
+  y: [72.2272,72.6946],
   mode: "lines",
   name: "SSG",
   line: {color:"#ffb81c", width: 4}
