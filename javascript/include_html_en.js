315a5306ef75cf0e3c419f44bcbccb006767d07f
dnyeum <dnyeum@gmail.com>
2026-03-25 06:26:19 +0000

    add enlgish blog section(Koreangit add .)

--- javascript/include_html_en.js
+++ javascript/include_html_en.js
@@ -1,6 +1,6 @@
 window.onload=function() {
 
-  var strHead = '<nav>\n  <ul>\n    <li>\n      <a href="https://psodds.com/en/index_en.html">Home</a>\n    </li>\n    <li><a href="https://psodds.com/en/GuideTo_en.html">User Guide</a>\n    </li>\n    <li>\n      <a href="https://psodds.com/en/2025_en.html">Season Summary</a>\n    </li>\n  </ul>\n</nav>'
+  var strHead = '<nav>\n  <ul>\n    <li>\n      <a href="https://psodds.com/">Home</a>\n    </li>\n    <li><a href="https://psodds.com/GuideTo.html>User Guide</a>\n    </li>\n    <li>\n      <a href="https://psodds.com/2025.html">Season Summary</a>\n    </li>\n    <li>\n      <a href="https://psodds.com/blog/index.html">Blog</a>\n    </li>  </ul>\n</nav>'
 
   var strYear = '<option value="none">-Season-</option>\n';
   for (var i=2023; i>=2001; i--) {
