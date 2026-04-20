14fa538d82511df339c6d404d682869b7ff327d5
PostseasonProb <dnyeum@hanmail.net>
2026-04-21 00:01:53 +0900

    2026.04.20

--- javascript/rank_table_en.js
+++ javascript/rank_table_en.js
@@ -1,5 +1,5 @@
 function rankst(){
-  var strRank='<h2> 2026 Projected Results </h2><p> Year to Date 04.19.2026</p>';
+  var strRank='<h2> 2026 Projected Results </h2><p> Year to Date 04.20.2026</p>';
   return strRank;
 }
 
